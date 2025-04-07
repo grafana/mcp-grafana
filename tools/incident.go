4e6e49bdfdcfeb94f27a9503857f8aeca1a74dd1
Dan O'Brien <daniel.obrien@grafana.com>
2025-04-07 11:24:15 -0400

    remove custom status

--- tools/incident.go
+++ tools/incident.go
@@ -12,7 +12,7 @@ import (
 type ListIncidentsParams struct {
 	Limit  int    `json:"limit" jsonschema:"description=The maximum number of incidents to return"`
 	Drill  bool   `json:"drill" jsonschema:"description=Whether to include drill incidents"`
-	Status string `json:"status" jsonschema:"description=The status of the incidents to include. Valid values: 'active', 'resolved', 'mitigated'"`
+	Status string `json:"status" jsonschema:"description=The status of the incidents to include. Valid values: 'active', 'resolved'"`
 }
 
 func listIncidents(ctx context.Context, args ListIncidentsParams) (*incident.QueryIncidentPreviewsResponse, error) {
