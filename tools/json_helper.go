edcd525cb032bce2262ce647ec09ca17482edd78
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2026-03-04 20:39:39 +0530

    chore(tools): modify response message

--- tools/json_helper.go
+++ tools/json_helper.go
@@ -11,7 +11,7 @@ func UnmarshalWithLimitMsg(data []byte, v any, bytesLimit int) error {
 	if err := json.Unmarshal(data, v); err != nil {
 		extraInfo := ""
 		if len(data) >= int(bytesLimit) {
-			extraInfo = "response size exceeds max memory limit , try with reduced query limits"
+			extraInfo = "response size exceeds limit , try querying in segments"
 		}
 		return fmt.Errorf("unmarshaling response: %w; %s", err, extraInfo)
 	}
